04800363d6078f4a345fe0008ba08e9e0726b456
vinhuchi <88415185+vinhuchi@users.noreply.github.com>
2022-06-25 03:24:51 +0700

    Update Main.lua

--- W-azure/Main.lua
+++ W-azure/Main.lua
@@ -1,3 +1,5 @@
 if game.PlaceId == 3101667897 then
    loadstring(game:HttpGet('https://raw.githubusercontent.com/vinhuchi/rblx/main/W-azure/LegendsOfSpeed.lua'))()
+elseif game.PlaceId == 2753915549 or game.PlaceId==4442272183 or  game.PlaceId == 7449423635 then
+   loadstring(game:HttpGet('https://raw.githubusercontent.com/vinhuchi/rblx/main/W-azure/BloxFruit.lua'))()
 end
