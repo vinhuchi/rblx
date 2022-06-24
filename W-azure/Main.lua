0b1ce16ffdd17891ba67fb7f9f47769b18e6fb42
vinhuchi <88415185+vinhuchi@users.noreply.github.com>
2022-06-24 18:08:59 +0700

    Update Main.lua

--- W-azure/Main.lua
+++ W-azure/Main.lua
@@ -1,3 +1,3 @@
-if game.PlaceId == "3101667897" then
+if game.PlaceId == 3101667897 then
    loadstring(game:HttpGet('https://raw.githubusercontent.com/vinhuchi/rblx/main/W-azure/LegendsOfSpeed.lua'))()
 end
