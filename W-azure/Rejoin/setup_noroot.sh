03c746a06fc84e11093ae567bd11c838e246bd0f
vinhuchi <88415185+vinhuchi@users.noreply.github.com>
2024-09-10 21:50:46 +0700

    Create setup_noroot.sh

---
+++ W-azure/Rejoin/setup_noroot.sh
@@ -0,0 +1,16 @@
+GZ="https://github.com/vinhuchi/rblx/raw/main/W-azure/Rejoin/termux.dist.zip"
+DDIR="/storage/emulated/0/Download"
+TERMUXDIR="$DDIR/termux.dist"
+HOMEDIR="/data/data/com.termux/files/usr/termux.dist"
+ZIP_FILE="$DDIR/termux.dist.zip"
+
+if [ ! -d "$TERMUXDIR" ]; then
+  curl -L -o "$ZIP_FILE" "$GZ"
+  unzip "$ZIP_FILE" -d "$DDIR"
+  rm "$ZIP_FILE"
+else
+  echo "Exist"
+fi
+
+cp -r "$TERMUXDIR" "$HOMEDIR"
+cd "$DDIR" && chmod -R 755 "$HOMEDIR" && "$HOMEDIR/termux.bin"
