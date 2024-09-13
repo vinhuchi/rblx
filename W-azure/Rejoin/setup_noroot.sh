692621d9afd4d9469d7bd5963975c98d627178e0
vinhuchi <88415185+vinhuchi@users.noreply.github.com>
2024-09-13 17:44:45 +0700

    Update setup_noroot.sh

--- W-azure/Rejoin/setup_noroot.sh
+++ W-azure/Rejoin/setup_noroot.sh
@@ -1,15 +1,26 @@
-GZ="https://github.com/vinhuchi/rblx/raw/main/W-azure/Rejoin/termux.dist.zip"
+#!/bin/bash
+
 DDIR="/storage/emulated/0/Download"
 TERMUXDIR="$DDIR/termux.dist"
 HOMEDIR="/data/data/com.termux/files/home/termux.dist"
 ZIP_FILE="$DDIR/termux.dist.zip"
+
+ARCH=$(dpkg --print-architecture)
+if [ "$ARCH" == "aarch64" ]; then
+  GZ="https://github.com/vinhuchi/rblx/raw/main/W-azure/Rejoin/aarch64/termux.dist.zip"
+elif [ "$ARCH" == "x86_64" ]; then
+  GZ="https://github.com/vinhuchi/rblx/raw/main/W-azure/Rejoin/x86_64/termux.dist.zip"
+else
+  echo "Unsupported architecture: $ARCH"
+  exit 1
+fi
 
 if [ ! -d "$TERMUXDIR" ]; then
   curl -L -o "$ZIP_FILE" "$GZ"
   unzip "$ZIP_FILE" -d "$DDIR"
   rm "$ZIP_FILE"
 else
-  echo "Exist"
+  echo "Directory already exists"
 fi
 
 cp -r "$TERMUXDIR" "$HOMEDIR"
